db7bb4a8540613c0a75eb76698659b6d589b3eb0
1w1111942IsmaelZamuz <70606903+1w1111942IsmaelZamuz@users.noreply.github.com>
2022-10-27 19:10:52 -0300

    Ahora funciona

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AltaComponentCliente } from './Cliente/alta/alta.component';
+import { BUscarEditarBorrar } from './Cliente/buscar-editar-borrar/buscar-editar-borrar.component';
 import { HomeComponent } from './home/home.component';
 import { ConsultarComponent } from './Oferta/consultar/consultar.component';
 import { AltaComponentProveedor } from './Proveedor/alta/alta.component';
@@ -9,6 +11,8 @@ const routes: Routes = [
   {path: "home", component:HomeComponent},
   {path: "registrarProveedor", component:AltaComponentProveedor},
   {path: "buscarProveedor", component:ConsultarComponentProveedor},
+  {path: "registrarCliente", component:AltaComponentCliente},
+  {path: "buscarCliente", component:BUscarEditarBorrar},
   {path: "ofertas", component:ConsultarComponent},
   {path:"", redirectTo:"home", pathMatch:"full"}
 ];
